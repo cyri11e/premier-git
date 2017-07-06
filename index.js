4d63f8ef25e27efdfa14152f93a168fd7b5e11b7
clabbe <cyrille.labbe@pharmagest.com>
2017-07-06 12:22:11 +0200

    integratino boot strap

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ let http = require('http')
 
 
 const server = http.createServer((req, res) => {
+    
   res.end();
 });
+
 server.on('clientError', (err, socket) => {
   socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
 });
